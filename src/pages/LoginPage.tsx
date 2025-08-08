13da881bc6acae795933480fbd2363cb70c90353
410am <rlclqk@naver.com>
2025-08-08 16:01:43 +0900

    feat : 구글 로그인, 토큰 발급

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,50 +1,3 @@
-// import {
-//   GoogleLogin as GoogleLoginButton,
-//   GoogleOAuthProvider,
-// } from "@react-oauth/google";
-// import axios from "axios";
-
-// const LoginPage = () => {
-//   const sendIdToken = async (idToken: string) => {
-//     try {
-//       console.log(idToken);
-//       const res = await axios.post("http://localhost:8080/auth/google", {
-//         idToken,
-//       });
-
-//       // ✅ 받은 JWT 저장
-//       const jwt = res.data.data.jwt;
-//       localStorage.setItem("accessToken", jwt);
-//       const token = localStorage.getItem("accessToken");
-//       console.log("🔥 res.data 전체:", res.data);
-//       console.log("🔥 token:", token);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   const GoogleLogin = () => {
-//     return (
-//       <GoogleLoginButton
-//         onSuccess={(credentialResponse) => {
-//           if (credentialResponse.credential) {
-//             sendIdToken(credentialResponse.credential);
-//           }
-//         }}
-//       />
-//     );
-//   };
-//   return (
-//     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}>
-//       <GoogleLogin />
-//     </GoogleOAuthProvider>
-//   );
-// };
-
-// export default LoginPage;
-
-// import { useEffect } from "react";
-
 import {
   GoogleLogin as GoogleLoginButton,
   GoogleOAuthProvider,
@@ -64,7 +17,8 @@ const LoginPage = () => {
         }
       );
 
-      const accessToken = res.data.accessToken;
+      const accessToken = res.data.data?.jwt;
+
       localStorage.setItem("accessToken", accessToken);
       console.log("🔥 accessToken 저장됨:", accessToken);
       window.location.href = "/login/success";
@@ -72,7 +26,6 @@ const LoginPage = () => {
       console.error("로그인 실패:", err);
     }
   };
-  window.location.href = "/login/success";
 
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}>
--- src/pages/LoginSuccessPage.tsx
+++ src/pages/LoginSuccessPage.tsx
@@ -1,27 +1,27 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 const LoginSuccessPage = () => {
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch("http://localhost:8080/login/success", {
-          credentials: "include", // ✅ 쿠키 포함 (refreshToken)
-        });
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const res = await fetch("http://localhost:8080/login/success", {
+  //         credentials: "include", // ✅ 쿠키 포함 (refreshToken)
+  //       });
 
-        const data = await res.json();
-        const accessToken = data.accessToken;
+  //       const data = await res.json();
+  //       const accessToken = data.accessToken;
 
-        if (accessToken) {
-          localStorage.setItem("accessToken", accessToken);
-          console.log("🔥 accessToken 저장됨:", accessToken);
-        }
-      } catch (err) {
-        console.error("accessToken 받기 실패", err);
-      }
-    })();
-  }, []);
+  //       if (accessToken) {
+  //         localStorage.setItem("accessToken", accessToken);
+  //         console.log("🔥 accessToken 저장됨:", accessToken);
+  //       }
+  //     } catch (err) {
+  //       console.error("accessToken 받기 실패", err);
+  //     }
+  //   })();
+  // }, []);
 
-  return <div>로그인 성공! </div>;
+  return <div>로그인 성공! {localStorage.getItem("accessToken")}</div>;
 };
 
 export default LoginSuccessPage;
